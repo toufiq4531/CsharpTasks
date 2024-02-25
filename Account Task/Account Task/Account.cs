67b6c8b5ab7563225a91fbd994bfc840d846b323
Mohammad Tofiqul Islam <118299820+toufiq4531@users.noreply.github.com>
2024-02-25 19:56:09 +0600

    Add files via upload

---
+++ Account Task/Account Task/Account.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Account_Task
+{
+    class Account
+    {
+        private string accName;
+        private string acid;
+        private double balance;
+
+        public Account()
+        {
+            accName = "Tofiqul";
+            acid = "49450";
+            balance = 200000;
+        }
+        public Account(string a, string b, double c)
+        {
+            this.accName = a;
+            this.acid = b;
+            this.balance = c;
+        }
+
+        public void setAccName(string m)
+        {
+            accName = m;
+        }
+
+        public void setAccId(string n)
+        {
+            acid = n;
+        }
+
+        public void setBalance(double o)
+        {
+            balance = o;
+        }
+
+        public string getAccName()
+        {
+            return accName;
+        }
+
+        public string getAccId()
+        {
+            return acid;
+        }
+
+        public double getBalance()
+        {
+            return balance;
+        }
+
+        public void deposit(double amount)
+        {
+            balance += amount;
+        }
+        public void withdraw(double amount)
+        {
+            balance -= amount;
+        }
+        public void transfer(double amount, Account receiver)
+        {
+            balance = this.balance - amount;
+            receiver.deposit(amount);
+        }
+
+        public void printInfo()
+        {
+            Console.WriteLine("Account Name: "+ accName);
+            Console.WriteLine("Account ID: "+ acid);
+            Console.WriteLine("Account Balance: "+ balance);
+        }
+    }
+}
---
+++ Account Task/Account Task/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Account_Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account a1 = new Account();
+            a1.printInfo();
+
+            Console.WriteLine();
+            
+            Account a2 = new Account();
+            a2.setAccName("Pranto");
+            a2.setAccId("42310");
+            a2.setBalance(500000);
+            a2.printInfo();
+
+            Console.WriteLine();
+            /*
+            a2.deposit(2345);
+            a2.printInfo();
+
+            Console.WriteLine();
+
+            a2.withdraw(1000);
+            a2.printInfo();
+            
+            Console.WriteLine();
+            */
+
+            a2.transfer(10000, a1);
+            Console.WriteLine(a1.getAccId()+ " Account Balance After Receiving: "+ a1.getBalance());
+            Console.WriteLine(a2.getAccId()+ " Account Balance After Transfering: "+ a2.getBalance());
+            
+            
+            Console.ReadKey();
+        }
+    }
+}
